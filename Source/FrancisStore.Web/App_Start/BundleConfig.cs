1e0ac20632f2085b6f40c8ce51b12b49057aa483
Duy Tran <francistranduy@live.com>
2019-08-16 14:24:32 +0700

    Add product page, separate css into shared styles and page styles.

--- Source/FrancisStore.Web/App_Start/BundleConfig.cs
+++ Source/FrancisStore.Web/App_Start/BundleConfig.cs
@@ -26,28 +26,49 @@ namespace FrancisStore.Web
             //          "~/Content/bootstrap.css",
             //          "~/Content/site.css"));
 
-            //Francis Store bundles
+            //Francis Store style bundles
             bundles.Add(new StyleBundle("~/Content/bootstrap").Include("~/Content/bootstrap.min.css"));
 
+            //Libary shared font styles
             bundles.Add(new StyleBundle("~/fonts/css").Include(
                 "~/fonts/Font-Awesome-4.7.0/css/font-awesome.min.css",
                 "~/fonts/themify-icons/themify-icons.css",
                 "~/fonts/Linearicons-Free-v1.0.0/style.css",
                 "~/fonts/elegant_font/style.css"));
 
-            bundles.Add(new StyleBundle("~/lib/css").Include(
-                "~/lib/animate.css/animate.min.css",
-                "~/lib/hamburgers/hamburgers.min.css",
-                "~/lib/animsition/css/animsition.min.css",
-                "~/lib/select2/css/select2.min.css",
-                "~/lib/bootstrap-daterangepicker/daterangepicker.min.css",
-                "~/lib/slick-carousel/slick.css",
-                "~/lib/lightbox2/css/lightbox.min.css"));
+            //Libary shared styles
+            bundles.Add(new StyleBundle("~/lib/animate.css").Include(
+                "~/lib/animate.css/animate.min.css"));
 
+            bundles.Add(new StyleBundle("~/lib/hamburgers").Include(
+                "~/lib/hamburgers/hamburgers.min.css"));
+
+            bundles.Add(new StyleBundle("~/lib/animsition").Include(
+                "~/lib/animsition/css/animsition.min.css"));
+
+            bundles.Add(new StyleBundle("~/lib/select2").Include(
+                "~/lib/select2/css/select2.min.css"));
+
+            bundles.Add(new StyleBundle("~/lib/slick-carousel").Include(
+                "~/lib/slick-carousel/slick.css"));
+
+            //Custom shared styles
             bundles.Add(new StyleBundle("~/Content/FrancisStore").Include(
                 "~/Content/util.css",
                 "~/Content/main.css"));
 
+            //Home styles
+            bundles.Add(new StyleBundle("~/lib/bootstrap-daterangepicker").Include(
+                "~/lib/bootstrap-daterangepicker/daterangepicker.min.css"));
+
+            bundles.Add(new StyleBundle("~/lib/lightbox2").Include(
+                "~/lib/lightbox2/css/lightbox.min.css"));
+
+            //Product styles
+            bundles.Add(new StyleBundle("~/lib/noUiSlider").Include(
+                "~/lib/noUiSlider/nouislider.min.css"));
+
+            //Francis Store script bundles
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                 "~/Scripts/jquery-{version}.js"));
 
@@ -69,6 +90,13 @@ namespace FrancisStore.Web
 
             bundles.Add(new ScriptBundle("~/bundles/sweetalert").Include(
                 "~/lib/sweetalert/sweetalert.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/daterangepicker").Include(
+                "~/lib/bootstrap-daterangepicker/moment.min.js",
+                "~/lib/bootstrap-daterangepicker/daterangepicker.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/noUiSlider").Include(
+                "~/lib/noUiSlider/nouislider.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/FrancisStore").Include(
                 "~/Scripts/slick-custom.js",
---
+++ Source/FrancisStore.Web/Controllers/ProductController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FrancisStore.Web.Controllers
+{
+    public class ProductController : FrancisStoreController
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        #region ProductPartialView
+        [ChildActionOnly]
+        public ActionResult Categories()
+        {
+            return PartialView("_Categories");
+        }
+
+        [ChildActionOnly]
+        public ActionResult Filters()
+        {
+            return PartialView("_Filters");
+        }
+
+        [ChildActionOnly]
+        public ActionResult Sorting()
+        {
+            return PartialView("_Sorting");
+        }
+
+        [ChildActionOnly]
+        public ActionResult Products()
+        {
+            return PartialView("_Products");
+        }
+
+        [ChildActionOnly]
+        public ActionResult Pagination()
+        {
+            return PartialView("_Pagination");
+        }
+        #endregion
+    }
+}
