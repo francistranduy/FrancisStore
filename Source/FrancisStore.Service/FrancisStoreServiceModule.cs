e9c475916931ec6591348f536f49066edc1b35f4
Duy Tran <francistranduy@live.com>
2019-09-11 16:49:23 +0700

    Add FrancisStore.Service project.

---
+++ Source/FrancisStore.Service/FrancisStoreServiceModule.cs
@@ -0,0 +1,24 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrancisStore.Service
+{
+    public class FrancisStoreServiceModule : Autofac.Module
+    {
+        public FrancisStoreServiceModule() { }
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            //Register the type-mapping, it configures which class end with "Repository" to instantiate for which interface implemented by itself.
+            builder.RegisterAssemblyTypes(System.Reflection.Assembly.GetExecutingAssembly())
+                .Where(type => type.Name.EndsWith("Service"))
+                .AsImplementedInterfaces();
+            base.Load(builder);
+        }
+    }
+}
