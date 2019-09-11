98bb189b2b29ceed91d5e29edb65d511f864f049
Duy Tran <francistranduy@live.com>
2019-09-11 17:17:59 +0700

    Update FrancisStore.Repository structure.

--- Source/FrancisStore.Repository/FrancisStoreRepositoryModule.cs
+++ Source/FrancisStore.Repository/FrancisStoreRepositoryModule.cs
@@ -8,15 +8,20 @@ using System.Threading.Tasks;
 
 namespace FrancisStore.Repository
 {
-    public class FrancisStoreDbModule: Module
+    public class FrancisStoreRepositoryModule: Module
     {
-        public FrancisStoreDbModule() {}
+        public FrancisStoreRepositoryModule() {}
 
         protected override void Load(ContainerBuilder builder)
         {
+            
             //Register the type-mapping, it configures which class to instantiate for which interface or base class
             builder.RegisterType<FrancisStoreDbContext>().As<IFrancisStoreDbContext>().InstancePerRequest();
-            builder.RegisterType<FrancisStoreRepository>().As<IFrancisStoreRepository>().InstancePerRequest();
+            //Register the type-mapping, it configures which class end with "Repository" to instantiate for which interface implemented by itself.
+            builder.RegisterAssemblyTypes(System.Reflection.Assembly.GetExecutingAssembly())
+                .Where(type => type.Name.EndsWith("Repository"))
+                .AsImplementedInterfaces();
+
             base.Load(builder);
         }
     }
--- Source/FrancisStore.Repository/Repositories/IFrancisStoreRepository.cs
+++ Source/FrancisStore.Repository/Repositories/IFrancisStoreRepository.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FrancisStore.Repository
+namespace FrancisStore.Repository.Repositories
 {
     public interface IFrancisStoreRepository: IDisposable
     {
