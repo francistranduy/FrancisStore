485bfd6f0fb95bb44541c249d82bf373f281ae4d
Duy Tran <francistranduy@live.com>
2019-10-10 13:09:01 +0700

    Add autofac module for FrancisStore.Identity

---
+++ Source/FrancisStore.Identity/FrancisStoreIdentityModule.cs
@@ -0,0 +1,28 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FrancisStore.Identity
+{
+    public class FrancisStoreIdentityModule : Autofac.Module
+    {
+        public FrancisStoreIdentityModule()
+        {
+        }
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterControllers(this.GetType().Assembly);
+
+            //Register the type-mapping, it configures which class end with "Service" to instantiate for which interface implemented by itself.
+            builder.RegisterAssemblyTypes(System.Reflection.Assembly.GetExecutingAssembly())
+                .Where(type => type.Name.EndsWith("Service"))
+                .AsImplementedInterfaces()
+                .InstancePerRequest();
+            base.Load(builder);
+        }
+    }
+}
