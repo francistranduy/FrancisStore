a39b8681f55d0c45ba0605e8bc44620841357bee
Duy Tran <francistranduy@live.com>
2019-10-10 13:18:05 +0700

    Update Item model in Service.

--- Source/FrancisStore.Service/Shopping/Models/Item.cs
+++ Source/FrancisStore.Service/Shopping/Models/Item.cs
@@ -9,8 +9,10 @@ namespace FrancisStore.Service.Shopping.Models
     public class Item
     {
         public long Id { get; set; }
+        public string Image { get; set; }
         public string Name { get; set; }
-        public Dictionary<string, string> Properties { get; set; }
+        public Dictionary<string, string> Options { get; set; }
+        public double Price { get; set; }
         public int Count { get; set; }
 
         public DateTime? DateCreated { get; set; }
